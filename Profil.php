1ac064d15da63c9ecc89394779d2e06a65f55636
Samuel Breuillet <breuillet.samuel@gmail.com>
2021-04-11 14:50:37 +0200

    Feature(form): create form to submit pseudo of the player

--- Profil.php
+++ Profil.php
@@ -1,8 +1,13 @@
 <?php
-/* Récupération du contenu du fichier .json */
-$contenu_fichier_json = file_get_contents('https://fortnite-api.com/v1/stats/br/v2/?name=lindsaycroco');
-/* Les données sont récupérées sous forme de tableau (true) */
-$tr = json_decode($contenu_fichier_json, true);
+$pseudo = '';
+if(isset($_GET['envoyer'])){
+    $pseudo = $_GET['pseudo'];
+
+    /* Récupération du contenu du fichier .json */
+    $contenu_fichier_json = file_get_contents('https://fortnite-api.com/v1/stats/br/v2/?name='.$pseudo);
+    /* Les données sont récupérées sous forme de tableau (true) */
+    $tr = json_decode($contenu_fichier_json, true);
+}
 ?>
  
 <!DOCTYPE html>
@@ -14,10 +19,27 @@ $tr = json_decode($contenu_fichier_json, true);
     </head>
  
     <body>
+
+        <h1>Boujour <?php echo $pseudo; ?></h1>
+
+        <form action="" method="get">
+            <input type="text" name="pseudo" value="" placeholder="Entrez votre pseudo" />
+            <input type="submit" name="envoyer" value="Envoyer" />
+        </form>
+
+        <?php 
+
+        if(isset($tr)) {
+        ?>
+
         <div class="level">
             <p>Bonjour</p> <?= $tr['data']['account']['name'].'<br>' ?> 
             <p>Tu es niveau : </p> <?= $tr['data']['battlePass']['level'].'<br>' ?>
             <p>Progression :</p><?= $tr['data']['battlePass']['progress'].'<br>' ?>
         </div>
+
+        <?php 
+        }
+        ?>
     </body>
 </html>
